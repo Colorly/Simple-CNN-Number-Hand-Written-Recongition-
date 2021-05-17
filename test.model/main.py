b707375b028c09a079d1c781ab9289c4c18346cd
Colorly <49200470+Colorly@users.noreply.github.com>
2021-05-16 22:40:16 -0400

    Update main.py

--- test.model/main.py
+++ test.model/main.py
@@ -24,6 +24,7 @@ b_test = keras.utils.to_categorical(b_test, 10)
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape))
 model.add(Conv2D(64, kernel_size=(3,3), activation ='relu'))
+model.add(Conv2D(128, kernel_size=(3,3), activation ='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
@@ -31,19 +32,12 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
+model.fit(a_train, b_train,batch_size=128,epochs=5,verbose=1)
 
-model.fit(a_train, b_train,
-          batch_size=128,
-          epochs=5,
-          verbose=1,
-          validation_split=0.2)
-
-score = model.evaluate(a_test, b_test, verbose = 0)
-print(score)
+results = model.evaluate(a_test, b_test, verbose = 0)
+print(results)
 
 model.save('test.model')
 
